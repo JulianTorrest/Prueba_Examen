59871f568efcc868a05f987635a012dcd879a454
JulianTorrest <43328220+JulianTorrest@users.noreply.github.com>
2025-03-30 15:26:40 -0500

    Update email_notifications.py

--- email_notifications.py
+++ email_notifications.py
@@ -1,6 +1,10 @@
 import os
 from mailersend import emails
 from config import MAILERSEND_API_KEY, MAILERSEND_DOMAIN
+import requests
+import json
+import logging
+
 
 MAILERSEND_API_KEY = "mlsn.550e6ccae0205cafda6f7e08c7373a1bf04b7e9b62cc1ca7100ba9254c60c034"
 MAILERSEND_DOMAIN = "trial-r83ql3pwjevgzw1j.mlsender.net"
