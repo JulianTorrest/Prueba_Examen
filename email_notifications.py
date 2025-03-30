50c32b71a611e66360fa65174c875b4c85914ef4
JulianTorrest <43328220+JulianTorrest@users.noreply.github.com>
2025-03-30 15:14:41 -0500

    Update email_notifications.py

--- email_notifications.py
+++ email_notifications.py
@@ -1,5 +1,7 @@
 import os
 from mailersend import emails
+from config import MAILERSEND_API_KEY, MAILERSEND_DOMAIN
+
 
 MAILERSEND_API_KEY = "mlsn.550e6ccae0205cafda6f7e08c7373a1bf04b7e9b62cc1ca7100ba9254c60c034"
 
@@ -7,11 +9,10 @@ def enviar_correo(destinatario, asunto, mensaje):
     mailer = emails.NewEmail(MAILERSEND_API_KEY)
     
     mail = {
-        "from": {"email": "mensaje@trial-r83ql3pwjevgzw1j.mlsender.net", "name": "Marketplace"},
+        "from": {"email": f"no-reply@trial-r83ql3pwjevgzw1j.mlsender.net", "name": "Marketplace"},
         "to": [{"email": destinatario}],
         "subject": asunto,
         "text": mensaje
     }
-
     response = mailer.send(mail)
     return response
